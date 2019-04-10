4b1b1c437fa65c293ddd7f77c7f8c104a0401e9e
Ángel Bravo Sáenz <angelbravosaenz@gmail.com>
2019-04-10 11:31:44 +0200

    primer commit

---
+++ build/CMakeFiles/iac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iac.dir/main.cpp.o"
+  "iac.pdb"
+  "iac"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
